9876ab5eeb154ccffb4ccd6dce74246b1a352384
Kamalchiranjeevi <84133425+Kamalchiranjeevi@users.noreply.github.com>
2025-01-15 21:09:59 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,103 @@
+#Q: check whether a character is a number or alphabet
+# print("Enter number or alphabet only")
+# character = input("Enter your character:")
+# if character in ['0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9']:
+#     print("character is number")
+# else:
+#     print("character is alphabet") 
+
+
+
+
+# Q: Find the largest of three numbers.
+# a, b, c = map(int,input().split())
+# if a > b and a > c:
+#     print("Maximum is a")
+# if b > a and b > c:
+#     print("Maximum is b")
+# if c > a and c > a:
+#     print("Maximum is c") 
+
+
+
+#Q: Check whether a given number is even or odd.
+# n = int(input("Enter a number: "))
+# if n > 0 and n % 2 == 0:
+#     print("Number is Even")
+# elif n > 0 and n % 2 == 1:
+#     print("Number is Odd")
+# else:
+#     print("You have not entered a valid number.")
+
+
+
+
+#Q: Given two numbers. Find which number is larger.
+# a, b = map(int, input("Enter 2 numbers: ").split())
+# if a > b:
+#     print("A is larger")
+# else:
+#     print("B is larger")
+
+
+
+
+
+#Q: Check for three numbers.Find which number is larger.
+# a, b, c = map(int,input("enter 3 numbers: ").split())
+# if a > b and a > c:
+#     print("a is max")
+# elif b > a and b > c:
+#     print("b is max")
+# else:
+#     print("c is max")
+
+
+
+
+# Q: Print Grades on the basis of marks.
+# n = int(input())
+# if n >= 90:
+#     print("Grade is A")
+# elif n >= 80:
+#     print("Garde is B")
+# elif n >= 70:
+#     print("Grade is C")
+# else:
+#     print("Grade is D")
+
+
+
+
+#Q: Design a simple Calculator.
+# a = int(input("Enter a number: "))
+# b = int(input("Enter a number: "))
+# operator = input("Enter operator: ")
+# if operator == "+":
+#     print(a + b)
+# elif operator == "-":
+#     print(a - b)
+# else:
+#     print(a * b)
+
+
+
+
+#Q: Perimeter and Area of a Circle and Rectangle.
+# print("1. Circle")
+# print("2. Rectangle")
+# a = int(input("Enter your choice 1 or 2: "))
+# if a == 1:
+#     radius = int(input("Enter radius: "))
+#     print("peri_of_cir = ", 2 * 3.14 * radius)
+#     print("area_of_cir = ", 3.14 * radius * radius) 
+# else:
+#     length = int(input("Enter length: "))
+#     width = int(input("Enter width: "))
+#     peri_of_rect = 2 * (length + width)
+#     area_of_rect = length * width
+#     print("peri_of_rect is: ", peri_of_rect)
+#     print("area_of_rect is: ", area_of_rect)
+
+
+
