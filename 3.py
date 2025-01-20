d51172e68eb5b5516553f673880c68e4b7f7ac42
Kamalchiranjeevi <84133425+Kamalchiranjeevi@users.noreply.github.com>
2025-01-20 21:37:06 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,119 @@
+#Q: Print table of a number.
+# num = int(input("Enter a number: "))
+# for i in range(1, 21):
+#     print(num,"x",i,"=",num*i)
+#     i += 1
+
+
+
+#Q: Print the sum of all natural numbers upto n.
+# num = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1,num+1):
+#     sum += i
+# print(sum)
+
+
+
+#Q: Print the sum of all odd natural numbers upto n.
+# num = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1,num+1):
+#     if i % 2 == 1:
+#         sum += i
+# print(sum)
+
+
+
+#Q: Print the numbers from n to 1.
+# num = int(input("Enter a number: "))
+# for i in range(1, num+1):
+#     print(num)
+#     num -= 1
+
+
+
+#Q: Print Factorial of a number.
+# num = int(input("Enter a number: "))
+# factorial = 1
+# for i in range(1, num+1):
+#     factorial = factorial * i
+# print("Factorial of",num,"is:",factorial)
+# """print(f"factorial of {num} is: ",factorial)"""
+
+
+
+#Q: Print table of a number. Using while loop
+# num = int(input("Enter a number: "))
+# a = 1
+# while num > 0:
+#     print(num ,"*" ,a ,"=" ,num*a)
+#     a += 1
+#     if a > num:
+#         break
+
+
+
+
+
+#Q: Print the sum of all natural numbers upto n.using while loop
+# num = int(input("Enter a number: "))
+# sum = 0
+# while num > 0:
+#     sum += num
+#     num -= 1
+# print(sum)
+
+
+
+#Q: Print the sum of all odd natural numbers upto n.using while loop
+# num = int(input("Enter a number: "))
+# sum = 0
+# while num > 0:
+#     if num % 2 == 1:
+#         sum += num
+#     num -= 1
+# print(sum)
+
+
+
+
+#Q: Print the numbers from n to 1. using while loop
+# num = int(input("Enter a number: "))
+# while num > 0:
+#     print(num)
+#     num -= 1
+
+
+
+
+#Q: Print Factorial of a number.using while loop
+# num = int(input("Enter a number: "))
+# factorial = 1
+# n = num
+# while num > 0:
+#     factorial *= num
+#     num -= 1
+# print("Factorial of",n,"is:",factorial)
+
+
+
+#Q: Print the powers of 2 that are smaller then number N.
+# num = int(input("Enter a number: "))
+# for i in range(0, num+1):
+#     if 2 ** i <= num:
+#         print(2**i)
+    
+
+
+
+
+#Q: Print the sum of digits of a given number.
+# num = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1, num+1):
+#     n = num % 10
+#     sum += n
+#     num //= 10 
+# print(sum)
+
